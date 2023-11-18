c0784c3d58e590e6c159aed2a72246de45e78810
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2023-11-18 23:52:31 +0000

    Basic changes

--- src/client/java/com/lbfalvy/ExampleModClient.java
+++ src/client/java/com/lbfalvy/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.lbfalvy;
 
 import net.fabricmc.api.ClientModInitializer;
 
--- src/main/java/com/lbfalvy/mixin/ExampleMixin.java
+++ src/main/java/com/lbfalvy/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.lbfalvy.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
